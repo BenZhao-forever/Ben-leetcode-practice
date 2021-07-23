5ab08ddcaa980921934201b4695fe11878eb113e
Ben Zhao <benjakezhao@gmail.com>
2021-07-22 18:13:55 -0700

    finish 258

--- src/main/java/leetcode258.java
+++ src/main/java/leetcode258.java
@@ -7,7 +7,18 @@ public class leetcode258 {
     public static void main(String[] args) {
         readMeSet.addnewline("https://leetcode.com/problems/add-digits/", 258);
     }
-    public static int addDigit(int num) {
+/*    public static int addDigit(int num) {
         return (num - 1) % 9 + 1;
+    }*/
+    public static int addDigits(int num) {
+        if (num < 10) {
+            return num;
+        }
+        int sum = 0;
+        while (num != 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return addDigits(sum);
     }
 }
