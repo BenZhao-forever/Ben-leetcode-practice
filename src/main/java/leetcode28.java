20bf59e85625007a6ff92ca7a82402e9bbe963d9
benzhao <benjakezhao@gmail.com>
2021-08-02 23:14:36 -0700

    change 28

--- src/main/java/leetcode28.java
+++ src/main/java/leetcode28.java
@@ -7,17 +7,51 @@ public class leetcode28 {
     public static void main(String[] args) {
         readMeSet.addnewline("https://leetcode.com/problems/implement-strstr/", 28);
     }
+//    public static int strStr(String haystack, String needle) {
+//        if (needle.length() == 0) {
+//            return 0;
+//        }
+//        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
+//            if (haystack.charAt(i) == needle.charAt(0)) {
+//                if (needle.equals(haystack.substring(i, i + needle.length()))) {
+//                    return i;
+//                }
+//            }
+//        }
+//        return -1;
+//    }
     public static int strStr(String haystack, String needle) {
         if (needle.length() == 0) {
             return 0;
         }
-        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
-            if (haystack.charAt(i) == needle.charAt(0)) {
-                if (needle.equals(haystack.substring(i, i + needle.length()))) {
-                    return i;
-                }
+        int[] next = makeNext(needle);
+        int j = 0;
+        for (int i = 0; i < haystack.length(); i++) {
+            while (j > 0 && haystack.charAt(i) != needle.charAt(j)) {
+                j = next[j - 1];
+            }
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                j++;
+            }
+            if (j == needle.length()) {
+                return i - needle.length() + 1;
             }
         }
         return -1;
+    }
+    public static int[] makeNext(String needle) {
+        int[] next = new int[needle.length()];
+        next[0] = 0;
+        int j = 0;
+        for (int i = 1; i < needle.length(); i++) {
+            while (j > 0 && needle.charAt(i) != needle.charAt(j)) {
+                j = next[j - 1];
+            }
+            if (needle.charAt(i) == needle.charAt(j)) {
+                j++;
+            }
+            next[i] = j;
+        }
+        return next;
     }
 }
--- src/test/java/leetcode28Test.java
+++ src/test/java/leetcode28Test.java
@@ -15,4 +15,9 @@ class leetcode28Test {
         assertEquals(-1, leetcode28.strStr("hello", "lol"));
         assertEquals(3, leetcode28.strStr("hello", "lo"));
     }
+
+    @Test
+    void makeNext() {
+        assertArrayEquals(new int[] {0,0,1,1,2,3,2,3}, leetcode28.makeNext("ABAABABA"));
+    }
 }
